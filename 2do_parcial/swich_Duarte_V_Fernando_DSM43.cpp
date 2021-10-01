be6d9720cfdcb7ce311dd60bd5f77800ae4e07b9
Fernando Duarte Villavicencio <al222010329@gmail.com>
2021-10-01 13:30:19 -0500

    Movimos en carpetas

---
+++ 2do_parcial/swich_Duarte_V_Fernando_DSM43.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char var;
+    cin>>var;
+switch (var)
+{
+    case 1:
+        cout<<"Escojiste el uno"<<endl;
+        break;
+    case 2:
+        cout<<"Escojiste el dos"<<endl;
+    break;
+    default:
+        break;
+}
+
+    return 0;
+    }
